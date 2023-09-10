69a6f451eff42a3a0290bc746bb531c8300a48bf
JongChan Choi <jong@chan.moe>
2023-09-11 01:08:21 +0900

    LeftSidebar 재사용 가능한 형태로 만듦

--- src/state/nav.ts
+++ src/state/nav.ts
@@ -1,17 +1,32 @@
 import { effect, signal } from "@preact/signals";
+import type { SystemVersion } from "~/type";
+
+const isServer = !globalThis.sessionStorage;
 
 export interface NavOpenStates {
   [slug: string]: boolean; // true: open, false: close
 }
 export const navOpenStatesSignal = signal<NavOpenStates>(
-  JSON.parse(globalThis.sessionStorage?.getItem("navOpenStates") || "{}"),
+  JSON.parse(globalThis.sessionStorage?.getItem("navOpenStates") || "{}")
 );
 effect(() => {
   const navOpenStates = navOpenStatesSignal.value;
   globalThis.sessionStorage?.setItem(
     "navOpenStates",
-    JSON.stringify(navOpenStates),
+    JSON.stringify(navOpenStates)
   );
 });
 
 export const slugSignal = signal<string>("");
+
+export const systemVersionSignal = signal(
+  isServer
+    ? "all"
+    : (globalThis.sessionStorage.getItem("systemVersion") as SystemVersion) ||
+        "v1"
+);
+
+effect(() => {
+  const systemVersion = systemVersionSignal.value;
+  globalThis.sessionStorage?.setItem("systemVersion", systemVersion);
+});
--- src/state/sidebar.ts
+++ src/state/sidebar.ts
@@ -1,17 +1,3 @@
-import { effect, signal } from "@preact/signals";
-import type { SystemVersion } from "~/type";
-
-const isServer = !globalThis.sessionStorage;
+import { signal } from "@preact/signals";
 
 export const sidebarOpenSignal = signal(false);
-export const systemVersionSignal = signal(
-  isServer
-    ? "all"
-    : (globalThis.sessionStorage.getItem("systemVersion") as SystemVersion) ||
-        "v1"
-);
-
-effect(() => {
-  const systemVersion = systemVersionSignal.value;
-  globalThis.sessionStorage?.setItem("systemVersion", systemVersion);
-});
