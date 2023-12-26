6094a16a3ed4dfed7304a0ba770a05d209cda0f2
Cosmo Shin (신의하) <me@xiniha.dev>
2023-12-26 21:57:02 +0900

    feat: 블로그 포스트 페이지 추가 (#28)
    Co-authored-by: JongChan Choi <jong@chan.moe>

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,6 +12,7 @@ const blogCollection = defineCollection({
     title: z.string(),
     description: z.string(),
     date: z.date().optional(),
+    author: z.string(),
     draft: z.boolean().optional(),
   }),
 });
