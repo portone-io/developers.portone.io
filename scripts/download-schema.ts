95562724770218d7e16c3bad7e55ee053335d072
JongChan Choi (Rieul) <rieul@chai.finance>
2024-01-07 23:12:30 -0800

    스키마 다운로드 스크립트 정비 (#283)

--- scripts/download-schema.ts
+++ scripts/download-schema.ts
@@ -12,7 +12,12 @@ import { getToken } from "https://deno.land/x/pbkit@v0.0.61/misc/github/index.ts
 import { open } from "https://deno.land/x/pbkit@v0.0.61/misc/browser.ts";
 import { render as renderGfm } from "https://deno.land/x/gfm@0.2.5/mod.ts";
 
-if (!import.meta.main) Deno.exit(1);
+const mdProperties = new Set([
+  "summary",
+  "description",
+  "x-portone-summary",
+  "x-portone-description",
+]);
 
 const schemas = ["V1 OpenAPI", "V2 OpenAPI", "V2 GraphQL"] as const;
 type Schema = (typeof schemas)[number];
@@ -22,27 +27,22 @@ const downloadFns: { [key in Schema]: () => Promise<void> } = {
   "V2 GraphQL": downloadV2Graphql,
 };
 
-const schema = await Input.prompt({
-  message: "내려받을 스키마를 선택해주세요.",
-  default: schemas[0],
-  list: true,
-  info: true,
-  suggestions: schemas as unknown as Schema[],
-}) as Schema;
+if (import.meta.main) {
+  const schema = await Input.prompt({
+    message: "내려받을 스키마를 선택해주세요.",
+    default: schemas[0],
+    list: true,
+    info: true,
+    suggestions: schemas as unknown as Schema[],
+  }) as Schema;
+  if (schema in downloadFns) await downloadFns[schema]();
+  else {
+    console.log("지원하지 않는 스키마입니다.");
+    Deno.exit(1);
+  }
+}
 
-const mdProperties = new Set([
-  "summary",
-  "description",
-  "x-portone-summary",
-  "x-portone-description",
-]);
-
-async function downloadV1Openapi() {
-  console.log(`스키마 위치: ${downloadV1Openapi.src}`);
-  console.log(`저장할 위치: ${downloadV1Openapi.dst}`);
-  console.log("내려받는 중...");
-  const res = await fetch(downloadV1Openapi.src);
-  const schema = await res.json();
+export function processV1Openapi(schema: any): any {
   traverseEveryProperty(schema, (node, property, context) => {
     if (property === "x-portone-per-pg") {
       context.renderAll = true;
@@ -53,21 +53,27 @@ async function downloadV1Openapi() {
     if (!context.renderAll && !property.startsWith("x-")) return;
     node[property] = renderGfm(node[property]);
   });
+  return schema;
+}
+
+export async function downloadV1Openapi() {
+  const src = "https://api.iamport.kr/api/docs";
+  // const src = "https://core-api.stg.iamport.co/api/docs";
+  const dst = import.meta.resolve("../src/schema/v1.openapi.json");
+  console.log(`스키마 위치: ${src}`);
+  console.log(`저장할 위치: ${dst}`);
+  console.log("내려받는 중...");
+  const res = await fetch(src);
+  const schema = processV1Openapi(await res.json());
   const json = JSON.stringify(schema, null, 2);
-  await touchAndSaveText(downloadV1Openapi.dst, json);
+  await touchAndSaveText(dst, json);
   console.log("완료");
 }
-// downloadV1Openapi.src = "https://api.iamport.kr/api/docs";
-downloadV1Openapi.src = "https://core-api.stg.iamport.co/api/docs";
-downloadV1Openapi.dst = import.meta.resolve("../src/schema/v1.openapi.json");
 
-async function downloadV2Openapi() {
-  const token = await ensureLoggedIn();
-  console.log(`스키마 위치: ${downloadV2Openapi.src}`);
-  console.log(`저장할 위치: ${downloadV2Openapi.dst}`);
-  console.log("내려받는 중...");
-  const yaml = await fetchTextFromGithub(downloadV2Openapi.src, token);
-  const schema = parseYaml(yaml);
+export function processV2Openapi(schema: any): any {
+  schema.servers = (schema.servers as { url: string }[]).filter((server) =>
+    !server.url.endsWith(".iamport.co")
+  );
   traverseEveryProperty(schema, (node, property) => {
     if (!node[property]) return;
     if (node[property]["x-portone-hidden"]) delete node[property];
@@ -88,40 +94,44 @@ async function downloadV2Openapi() {
     if (!mdProperties.has(property)) return;
     node[property] = renderGfm(node[property]);
   });
-  const json = JSON.stringify(schema, null, 2);
-  await touchAndSaveText(downloadV2Openapi.dst, json);
-  console.log("완료");
+  return schema;
 }
-downloadV2Openapi.src =
-  "https://raw.githubusercontent.com/portone-io/public-api-service/main/schema/openapi.yml";
-downloadV2Openapi.dst = import.meta.resolve("../src/schema/v2.openapi.json");
 
-async function downloadV2Graphql() {
+export async function downloadV2Openapi() {
+  const src =
+    "https://raw.githubusercontent.com/portone-io/public-api-service/main/schema/openapi.yml";
+  const dst = import.meta.resolve("../src/schema/v2.openapi.json");
   const token = await ensureLoggedIn();
-  console.log(`스키마 위치: ${downloadV2Graphql.src}`);
-  console.log(`저장할 위치: ${downloadV2Graphql.dst}`);
+  console.log(`스키마 위치: ${src}`);
+  console.log(`저장할 위치: ${dst}`);
   console.log("내려받는 중...");
-  const graphql = await fetchTextFromGithub(downloadV2Graphql.src, token);
-  await touchAndSaveText(downloadV2Graphql.dst, graphql);
+  const yaml = await fetchTextFromGithub(src, token);
+  const schema = processV2Openapi(parseYaml(yaml));
+  const json = JSON.stringify(schema, null, 2);
+  await touchAndSaveText(dst, json);
   console.log("완료");
 }
-downloadV2Graphql.src =
-  "https://raw.githubusercontent.com/portone-io/public-api-service/main/schema/schema.graphql";
-downloadV2Graphql.dst = import.meta.resolve("../src/schema/v2.graphql");
 
-if (schema in downloadFns) await downloadFns[schema]();
-else {
-  console.log("지원하지 않는 스키마입니다.");
-  Deno.exit(1);
+export async function downloadV2Graphql() {
+  const src =
+    "https://raw.githubusercontent.com/portone-io/public-api-service/main/schema/schema.graphql";
+  const dst = import.meta.resolve("../src/schema/v2.graphql");
+  const token = await ensureLoggedIn();
+  console.log(`스키마 위치: ${src}`);
+  console.log(`저장할 위치: ${dst}`);
+  console.log("내려받는 중...");
+  const graphql = await fetchTextFromGithub(src, token);
+  await touchAndSaveText(dst, graphql);
+  console.log("완료");
 }
 
-async function touchAndSaveText(resolvedPath: string, text: string) {
+export async function touchAndSaveText(resolvedPath: string, text: string) {
   const url = new URL(resolvedPath);
   await ensureFile(url);
   await Deno.writeTextFile(url, text);
 }
 
-async function fetchTextFromGithub(url: string, token: string) {
+export async function fetchTextFromGithub(url: string, token: string) {
   const res = await fetch(url, {
     headers: {
       Accept: "application/vnd.github.v3.raw",
@@ -131,7 +141,7 @@ async function fetchTextFromGithub(url: string, token: string) {
   return await res.text();
 }
 
-async function ensureLoggedIn() {
+export async function ensureLoggedIn() {
   try {
     const token = await getToken();
     return token;
@@ -156,7 +166,7 @@ async function ensureLoggedIn() {
   }
 }
 
-function traverseEveryProperty(
+export function traverseEveryProperty(
   object: any,
   fn: (node: any, property: string, context: any) => void,
   context: any = {},
