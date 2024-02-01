64bc24b0dd744a77910b7bc79f8dda2eb02c7f08
XiNiHa <me@xiniha.dev>
2024-02-01 15:33:29 +0900

    API 문서에서 버전토글 눌렀을 때 메인 페이지로 안 튕겨나가게 수정

--- src/layouts/gnb/VersionSwitch.tsx
+++ src/layouts/gnb/VersionSwitch.tsx
@@ -2,17 +2,32 @@ import { useServerFallback } from "~/misc/useServerFallback";
 import { systemVersionSignal } from "~/state/nav";
 import type { SystemVersion } from "~/type";
 
+const pathMappings = {
+  "/api/rest-v1": "/api/rest-v2",
+  "/api/rest-v2": "/api/rest-v1",
+};
+
+const hiddenPaths = ["/release-notes"];
+
 export interface VersionSwitchProps {
+  url: string;
   className?: string;
 }
-export function VersionSwitch({ className }: VersionSwitchProps) {
+export function VersionSwitch({ url, className }: VersionSwitchProps) {
+  if (hiddenPaths.some((path) => new URL(url).pathname.startsWith(path)))
+    return null;
+
   const systemVersion = systemVersionSignal.value;
   return (
     <div
       style={{ transition: "margin 0.1s" }}
       onClick={() => {
         systemVersionSignal.value = systemVersion !== "v1" ? "v1" : "v2";
-        if (!location.pathname.startsWith("/docs/")) location.href = "/";
+        if (location.pathname.startsWith("/docs/")) return;
+        location.href =
+          Object.entries(pathMappings).find(([from]) =>
+            location.pathname.startsWith(from),
+          )?.[1] ?? "/";
       }}
       class={`bg-slate-1 border-slate-3 text-12px text-slate-5 p-1px border-1 flex cursor-pointer select-none overflow-hidden whitespace-pre rounded-[6px] text-center font-bold ${
         className || ""
