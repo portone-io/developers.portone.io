f4c8143146acd73b23a292882e476a729a70f918
Jaemin Yu (Holden) <48317457+youjmen@users.noreply.github.com>
2024-02-26 20:25:23 +0900

    feat: paypal v2 core 개발자 센터 문서 작성 (#302)
    Co-authored-by: Ashley <ashley@chai.finance>
    Co-authored-by: XiNiHa <me@xiniha.dev>

---
+++ src/components/gitbook/VersionGate.tsx
@@ -0,0 +1,26 @@
+import type React from "react";
+import { useServerFallback } from "~/misc/useServerFallback";
+import { systemVersionSignal } from "~/state/nav";
+
+interface Props {
+  default: "v1" | "v2";
+  v1?: React.ReactNode;
+  v2?: React.ReactNode;
+  children?: React.ReactNode;
+}
+
+export default function VersionGate(props: Props) {
+  const systemVersion = useServerFallback(systemVersionSignal.value, "all");
+
+  const v1Content =
+    props.default === "v1" ? props.v1 ?? props.children : props.v1;
+  const v2Content =
+    props.default === "v2" ? props.v2 ?? props.children : props.v2;
+
+  return (
+    <>
+      {systemVersion !== "v1" && v2Content}
+      {systemVersion !== "v2" && v1Content}
+    </>
+  );
+}
