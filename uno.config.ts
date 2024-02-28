bd19a51e6bebf8281fd5c721a36d2acb8733f0ae
XiNiHa <me@xiniha.dev>
2024-02-28 21:44:54 +0900

    ESLint 셋업
    GitHub Actions 린트 추가
    
    ESLint 설정에서 일반 마크다운 파일 제외
    
    각종 린트 컨픽 이슈 수정

--- uno.config.ts
+++ uno.config.ts
@@ -1,20 +1,19 @@
+import presetIcons from "@unocss/preset-icons";
+import presetWind from "@unocss/preset-wind";
 import {
   defineConfig,
   transformerDirectives,
   transformerVariantGroup,
 } from "unocss";
-import presetIcons from "@unocss/preset-icons";
-import presetWind from "@unocss/preset-wind";
-import tailwindConfig from "./tailwind.config.cjs";
 
 export default defineConfig({
-  // TODO: Upgrade unocss
-  presets: [presetIcons(), presetWind() as any],
+  presets: [presetIcons(), presetWind()],
   transformers: [transformerDirectives(), transformerVariantGroup()],
-  ...tailwindConfig,
   theme: {
-    colors: {
-      portone: "#FC6B2D",
+    extend: {
+      colors: {
+        portone: "#FC6B2D",
+      },
     },
   },
 });
