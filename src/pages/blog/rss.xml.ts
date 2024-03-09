19251751094e5b8f6883b2520f31b7f3524378dd
Cosmo Shin (신의하) <me@xiniha.dev>
2024-03-09 17:21:39 +0900

    description에 마침표 추가

--- src/pages/blog/rss.xml.ts
+++ src/pages/blog/rss.xml.ts
@@ -7,7 +7,7 @@ export async function GET(context: APIContext) {
   return rss({
     title: "PortOne 기술 블로그",
     description:
-      "핀테크 기업 포트원이 기술을 통해 결제를 혁신해나가는 과정을 이야기합니다",
+      "핀테크 기업 포트원이 기술을 통해 결제를 혁신해나가는 과정을 이야기합니다.",
     site: context.site!,
     items: blog
       .filter((post) => !post.data.draft)
