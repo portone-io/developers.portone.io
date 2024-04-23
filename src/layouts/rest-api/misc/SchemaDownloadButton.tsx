b554ad48d09027d01f05d6b53b588663dce098f6
CirnoV <cirnov59@gmail.com>
2024-04-23 21:18:23 +0900

    Astro endpoint 기능을 이용한 정적 파일 링크로 재구현

--- src/layouts/rest-api/misc/SchemaDownloadButton.tsx
+++ src/layouts/rest-api/misc/SchemaDownloadButton.tsx
@@ -1,73 +1,31 @@
 import type React from "preact/compat";
-import { createRef } from "preact/compat";
-
-import { getEveryEndpoints } from "../schema-utils/endpoint";
-import {
-  getOperation,
-  isQueryOrBodyOperation,
-} from "../schema-utils/operation";
 
 export interface SchemaDownloadButtonProps {
   href: string;
   label: React.ReactNode;
   children: React.ReactNode;
+  download: string;
 }
 export default function SchemaDownloadButton({
   href,
   label,
   children,
+  download,
 }: SchemaDownloadButtonProps) {
-  const downloadRef = createRef<HTMLAnchorElement>();
   return (
     <div class="flex flex-col items-start gap-1 text-14px">
       <a
-        ref={downloadRef}
-        class="hidden"
-        download="portone-api-schema.json"
+        download={download}
         target="_blank"
-      ></a>
-      <button
-        type="button"
+        href={href}
         class="inline-flex items-center gap-2 rounded bg-slate-1 p-2 pr-3 font-bold hover:bg-slate-2"
-        onClick={() =>
-          void (async () => {
-            const schema = await downloadSchema(href);
-            const blob = new Blob([JSON.stringify(schema)], {
-              type: "application/json",
-            });
-            const url = URL.createObjectURL(blob);
-            const a = downloadRef.current!;
-            a.href = url;
-            a.click();
-          })()
-        }
       >
         <i class="i-ic-baseline-download text-xl" />
         {label}
-      </button>
+      </a>
       {children}
     </div>
   );
-}
-
-async function downloadSchema(href: string) {
-  const response = await fetch(href);
-  const schema = (await response.json()) as unknown;
-  getEveryEndpoints(schema)
-    .map((endpoint) => getOperation(schema, endpoint))
-    .filter(({ parameters }) => parameters !== undefined)
-    .filter(isQueryOrBodyOperation)
-    .forEach((operation) => {
-      delete operation.requestBody;
-      operation.parameters!.forEach((parameter) => {
-        if ("x-portone-query-or-body" in parameter) {
-          const { required } = parameter["x-portone-query-or-body"]!;
-          if (required) parameter.required = true;
-          delete parameter["x-portone-query-or-body"];
-        }
-      });
-    });
-  return schema;
 }
 
 export interface PostmanGuideProps {
--- src/pages/api/rest-v2/_Page.tsx
+++ src/pages/api/rest-v2/_Page.tsx
@@ -35,7 +35,8 @@ export default function RestV2(props: RestV2Props) {
       <prose.p>
         <SchemaDownloadButton
           label="OpenAPI JSON 내려받기"
-          href="https://raw.githubusercontent.com/portone-io/developers.portone.io/main/src/schema/v2.openapi.json"
+          href="/api/schema/v2.openapi.json"
+          download="portone-v2-openapi.json"
         >
           <PostmanGuide href="https://learning.postman.com/docs/integrations/available-integrations/working-with-openAPI/" />
         </SchemaDownloadButton>
