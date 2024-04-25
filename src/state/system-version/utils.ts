fd5db7bb682b056a1912e9637f64aaa94db3b540
XiNiHa <me@xiniha.dev>
2024-04-25 22:56:13 +0900

    SystemVersion 기본값 v2로 설정

--- src/state/system-version/utils.ts
+++ src/state/system-version/utils.ts
@@ -3,5 +3,5 @@ import type { SystemVersion } from "~/type";
 export const parseSystemVersion = (value: unknown) => {
   return ["v1", "v2"].includes(value as string)
     ? (value as SystemVersion)
-    : "v1"; // default
+    : "v2"; // default
 };
