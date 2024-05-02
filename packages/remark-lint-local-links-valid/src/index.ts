967d524033bc84a3f5a319de3e418f5952de4543
CirnoV (Sickle) <cirnov59@gmail.com>
2024-05-02 16:23:39 +0900

    remark-lint-local-links-valid 모노레포화 (#434)
    Co-authored-by: Cosmo Shin (신의하) <me@xiniha.dev>

---
+++ packages/remark-lint-local-links-valid/src/index.ts
@@ -0,0 +1,82 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
+import { lintRule } from "unified-lint-rule";
+import { visit } from "unist-util-visit";
+
+interface Options {
+  baseDir: string;
+  excludePaths: string[];
+  redirects: Record<string, string>;
+}
+const remarkLintLocalLinksValid = lintRule(
+  "remark-lint:local-links-valid",
+  async (tree, file, options: Partial<Options>) => {
+    if (!options.baseDir) {
+      throw new Error("Missing required option `baseDir`");
+    }
+    const baseDir = path.resolve(options.baseDir);
+    const filePath = path.resolve(file.cwd, file.history[0] ?? "");
+    const excludePaths =
+      options.excludePaths?.map((p) => path.join(baseDir, p)) ?? [];
+    const redirects = options.redirects ?? {};
+    const tasks: Promise<void>[] = [];
+    visit(tree, (node) => {
+      if (
+        "url" in node &&
+        typeof node.url === "string" &&
+        node.type === "link" &&
+        isLocalLink(node.url)
+      ) {
+        const url = node.url.split(/[#?]/)[0] || "";
+        let absPath = "";
+        if (path.isAbsolute(url)) {
+          absPath = path.join(baseDir, url);
+        } else {
+          absPath = path.join(path.dirname(filePath), url);
+        }
+        const resolvedPath = resolveRedirect(
+          redirects,
+          path.relative(baseDir, absPath),
+        );
+        if (isLocalLink(resolvedPath) === false) {
+          return;
+        }
+        absPath = path.join(baseDir, resolvedPath);
+        if (excludePaths.some((p) => absPath.startsWith(p))) {
+          return;
+        }
+        if (path.extname(absPath) !== "") {
+          file.message("Local link should not have an extension", node);
+          return;
+        }
+        const task = Promise.any(
+          [".md", ".mdx"].map((ext) => fs.access(absPath + ext)),
+        ).catch(() => {
+          file.message(`File not found: ${absPath}`, node);
+        });
+        tasks.push(task);
+      }
+    });
+    await Promise.all(tasks);
+  },
+);
+const isLocalLink = (url: string): boolean => {
+  try {
+    new URL(url);
+  } catch {
+    return true;
+  }
+  return false;
+};
+const resolveRedirect = (
+  redirects: Record<string, string>,
+  url: string,
+): string => {
+  let resolved = url;
+  while (redirects[resolved]) {
+    resolved = redirects[resolved]?.split(/[#?]/)[0] ?? resolved;
+  }
+  return resolved;
+};
+export default remarkLintLocalLinksValid;
