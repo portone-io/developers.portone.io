ed9f5e0bc2398d66ff4ad0ce3cebfb43311c230a
XiNiHa <me@xiniha.dev>
2024-05-23 05:49:42 +0900

    SolidStart 셋업 및 /opengraph.png 라우트 포팅

---
+++ src/entry-client.tsx
@@ -0,0 +1,6 @@
+// @refresh reload
+import "uno.css";
+
+import { mount, StartClient } from "@solidjs/start/client";
+
+mount(() => <StartClient />, document.getElementById("app")!);
--- src/routes/opengraph.png.ts
+++ src/routes/opengraph.png.ts
@@ -1,8 +1,6 @@
-import type { APIRoute } from "astro";
-
 import { generate } from "~/misc/opengraph/image-generator";
 
-export const GET: APIRoute = async () => {
+export const GET = async () => {
   const response = await generate({});
   return new Response(response, {
     status: 200,
