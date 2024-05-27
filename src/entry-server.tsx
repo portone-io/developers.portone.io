d5bfa2871e76e7aa9c4a326a698be55f8287e3a9
XiNiHa <me@xiniha.dev>
2024-05-27 19:43:13 +0900

    잘못된 favicon 경로 수정

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -8,7 +8,7 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" type="image/png" href="/favicon.png" />
           {assets}
         </head>
         <body>
