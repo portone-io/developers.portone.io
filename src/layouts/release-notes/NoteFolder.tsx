cfc6364af79b3acadc2c92b92389801457d86753
CirnoV (Sickle) <cirnov59@gmail.com>
2024-05-27 19:08:32 +0900

    릴리즈 노트 사이드바 접는 메뉴 구현 (#482)
    Co-authored-by: Iha Shin (Cosmo, 신의하) <me@xiniha.dev>

---
+++ src/layouts/release-notes/NoteFolder.tsx
@@ -0,0 +1,49 @@
+import { useSignal } from "@preact/signals";
+import clsx from "clsx";
+import type { PropsWithChildren } from "preact/compat";
+
+import { getLinkStyle, LinkTitle } from "../sidebar/LeftSidebarItem";
+
+interface FolderProps {
+  title: string;
+  path: string;
+  activeSlug: string | undefined;
+}
+
+function NoteFolder({
+  title,
+  path,
+  activeSlug,
+  children,
+}: FolderProps & PropsWithChildren) {
+  const openSignal = useSignal(activeSlug?.startsWith(path));
+  const open = openSignal.value;
+  return (
+    <div>
+      <button
+        class={clsx("flex w-full pr-0", getLinkStyle(false))}
+        onClick={() => {
+          openSignal.value = !open;
+        }}
+      >
+        <div class="grow">
+          <LinkTitle title={title} />
+        </div>
+        <div class="h-full flex items-center p-2">
+          <i
+            class={`inline-block ${
+              open
+                ? "i-ic-baseline-keyboard-arrow-down"
+                : "i-ic-baseline-keyboard-arrow-right"
+            }`}
+          />
+        </div>
+      </button>
+      <div class={`${open ? "block" : "hidden"} pl-2`}>
+        <ul class="flex flex-col gap-1 border-l pl-2">{children}</ul>
+      </div>
+    </div>
+  );
+}
+
+export default NoteFolder;
