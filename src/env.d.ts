5f109ef59a8095daf857bcb0901498533728a203
XiNiHa <me@xiniha.dev>
2024-05-27 20:15:12 +0900

    아일랜드 관련 JSX attribute 타입 추가

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,11 @@
 /// <reference types="@solidjs/start/env" />
 
 import "@total-typescript/ts-reset/filter-boolean";
+
+declare module "solid-js" {
+  namespace JSX {
+    interface IntrinsicAttributes {
+      "client:idle"?: boolean;
+    }
+  }
+}
