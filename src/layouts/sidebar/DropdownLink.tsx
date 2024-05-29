dbcd712d85e7cdd0dfe3020fac21bfa1a54badc4
CirnoV <cirnov59@gmail.com>
2024-05-30 06:14:34 +0900

    개발자문서 모바일 링크 구현

---
+++ src/layouts/sidebar/DropdownLink.tsx
@@ -0,0 +1,48 @@
+import { useSignal } from "@preact/signals";
+import clsx from "clsx";
+import type React from "preact/compat";
+
+export interface DropdownLinkProps {
+  children: React.ReactNode;
+  items: DropdownItem[];
+}
+export interface DropdownItem {
+  label: React.ReactNode;
+  link: string;
+}
+export default function DropdownLink({ children, items }: DropdownLinkProps) {
+  const showItemsSignal = useSignal(false);
+  return (
+    <div class="relative h-full w-full flex flex-col cursor-default">
+      <button
+        class="flex flex-1 items-center gap-2 border-1 border-slate-3 rounded-6px p-2 px-4 text-slate-600"
+        onClick={() => (showItemsSignal.value = !showItemsSignal.value)}
+      >
+        {children}
+        <div class="flex-1" />
+        <i class="i-ic-baseline-keyboard-arrow-up text-xl"></i>
+      </button>
+      <div class="relative z-[12] w-full">
+        {showItemsSignal.value && (
+          <div class="absolute w-full flex flex-col border bg-white py-2 shadow-lg">
+            {items.map(({ label, link }, i) => (
+              <a
+                key={i}
+                class="inline-flex items-center gap-2 px-4 py-2 hover:bg-slate-1"
+                href={link}
+              >
+                <i
+                  class={clsx(
+                    "i-ic-baseline-check",
+                    location.pathname.startsWith(link) || "opacity-0",
+                  )}
+                />
+                <span>{label}</span>
+              </a>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
