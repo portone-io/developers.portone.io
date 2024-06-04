bc64761d27d16efcadc22f2efb83e905db4e33fe
XiNiHa <me@xiniha.dev>
2024-06-04 19:34:07 +0900

    아일랜드 비활성화

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,3 @@
 /// <reference types="@solidjs/start/env" />
 
 import "@total-typescript/ts-reset/filter-boolean";
-
-declare module "solid-js" {
-  namespace JSX {
-    interface IntrinsicAttributes {
-      "client:idle"?: boolean;
-    }
-  }
-}
