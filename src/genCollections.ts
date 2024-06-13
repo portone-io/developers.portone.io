17bcbcd21d3711dac0895e5f5817f926cb494e58
XiNiHa <me@xiniha.dev>
2024-06-13 20:16:22 +0900

    Collection 스크립트가 watch 모드에서 업데이트된 파일만 갱신하도록 개선

--- src/genCollections.ts
+++ src/genCollections.ts
@@ -7,35 +7,44 @@ import jsYaml from "js-yaml";
 
 import { type CollectionConfig } from "./content/config";
 
-async function getCollection(name: string, config: CollectionConfig) {
-  const allPaths = await fastGlob(`./content/${config.path}/**/*.mdx`, {
-    cwd: import.meta.dirname,
-  });
-  const paths = allPaths.filter((path) => !path.includes("/_"));
+type Collection = {
+  files: Set<string>;
+  entries: Map<string, CollectionEntry>;
+};
+
+type CollectionEntry = {
+  slug: string;
+  frontmatter: unknown;
+  file: string;
+};
+
+async function getCollection(
+  config: CollectionConfig,
+  changedFiles?: Set<string>,
+): Promise<Collection> {
+  let files = changedFiles;
+  if (!files) {
+    const allFiles = await fastGlob(`content/${config.path}/**/*.mdx`, {
+      cwd: import.meta.dirname,
+    });
+    files = new Set(allFiles.filter((path) => !path.includes("/_")));
+  }
+
   const slugRegex = new RegExp(`^.*?/${config.path}/([\\s\\S]+)\\.mdx$`);
 
   const entries = await Promise.all(
-    paths.map(async (file) => {
-      const slug = slugRegex.exec(file)?.[1];
+    [...files].map(async (file) => {
+      const slug = slugRegex.exec(file)?.[1] ?? "";
       const content = await fs.readFile(
         path.join(import.meta.dirname, file),
         "utf-8",
       );
       const frontmatter = getFrontmatter(config, file, content);
-      return {
-        slug,
-        code: `{
-  slug: ${JSON.stringify(slug)},
-  frontmatter: ${JSON.stringify(frontmatter, null, 2).replace(/\n/g, "\n  ")},
-  load: () => import(${JSON.stringify(`~/${file}`)}),
-}`,
-      };
+      return [slug, { slug, frontmatter, file }] as const;
     }),
   );
 
-  return `export const ${name} = {
-${entries.map((entry) => `  ${JSON.stringify(entry.slug)}: ${entry.code.replace(/\n/g, "\n  ")},`).join("\n")}
-}`;
+  return { files, entries: new Map(entries) };
 }
 
 function getFrontmatter(
@@ -49,18 +58,67 @@ function getFrontmatter(
   } else throw new Error(`Frontmatter not found in ${path}`);
 }
 
-async function generate() {
+async function generate(
+  previous?: Map<string, Collection>,
+  changedFilesMap?: Map<string, Set<string>>,
+) {
   const { config } = (await import(
     `./content/config?t=${Date.now()}`
   )) as typeof import("./content/config");
 
-  const collections = await Promise.all(
-    Object.entries(config).map(([name, config]) => getCollection(name, config)),
-  );
+  const collections = new Map<string, Collection>();
+
+  if (previous && changedFilesMap) {
+    for (const [name, collection] of previous) {
+      if (!Object.keys(changedFilesMap).includes(name)) {
+        collections.set(name, collection);
+      }
+    }
+    for (const [name, files] of changedFilesMap) {
+      const updated = await getCollection(
+        config[name as keyof typeof config],
+        files,
+      );
+      collections.set(name, {
+        files: new Set([
+          ...(previous.get(name)?.files ?? []),
+          ...updated.files,
+        ]),
+        entries: new Map([
+          ...(previous.get(name)?.entries ?? []),
+          ...updated.entries,
+        ]),
+      });
+    }
+  } else {
+    const entries = await Promise.all(
+      Object.entries(config).map(
+        async ([name, config]) => [name, await getCollection(config)] as const,
+      ),
+    );
+
+    for (const [name, collection] of entries) {
+      collections.set(name, collection);
+    }
+  }
 
   const content = `// prettier-ignore
 
-${collections.join("\n\n")}
+${[...collections]
+  .map(
+    ([name, c]) => `export const ${name} = {
+${[...c.entries.values()]
+  .map(
+    (entry) => `  ${JSON.stringify(entry.slug)}: {
+    slug: ${JSON.stringify(entry.slug)},
+    frontmatter: ${JSON.stringify(entry.frontmatter, null, 2).replace(/\n/g, "\n    ")},
+    load: () => import(${JSON.stringify(`~/${entry.file}`)}),
+  },`,
+  )
+  .join("\n")}
+}`,
+  )
+  .join("\n\n")}
 `;
 
   const file = path.join(
@@ -70,13 +128,61 @@ ${collections.join("\n\n")}
 
   await fs.mkdir(path.dirname(file), { recursive: true });
   await fs.writeFile(file, content);
+
+  return { collections };
 }
 
 if (process.argv[2] === "watch") {
-  await generate();
+  let collections: Map<string, Collection> | undefined;
+
+  try {
+    const result = await generate();
+    collections = result.collections;
+  } catch (e) {
+    console.error(e);
+  }
+
   await subscribe(
     path.join(import.meta.dirname, "./content"),
-    () => generate(),
+    async (error, events) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      let changedFilesMap: Map<string, Set<string>> | undefined;
+
+      if (collections) {
+        changedFilesMap = new Map();
+
+        for (const event of events) {
+          if (event.type === "update") {
+            for (const [name, collection] of collections) {
+              const relativePath = path.relative(
+                import.meta.dirname,
+                event.path,
+              );
+              if (collection.files.has(relativePath)) {
+                if (!changedFilesMap.has(name)) {
+                  changedFilesMap.set(name, new Set());
+                }
+                changedFilesMap.get(name)?.add(relativePath);
+              }
+            }
+          } else {
+            changedFilesMap = undefined;
+            break;
+          }
+        }
+      }
+
+      try {
+        const result = await generate(collections, changedFilesMap);
+        collections = result.collections;
+      } catch (e) {
+        console.error(e);
+      }
+    },
     { ignore: ["__generated__"] },
   );
 } else {
