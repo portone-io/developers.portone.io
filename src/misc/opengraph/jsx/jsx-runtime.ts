8a4be8a9e8a29e7ae80503065fd8fe8c46d9684f
XiNiHa <me@xiniha.dev>
2024-07-13 15:15:36 +0900

    Satori용 JSX 런타임에서 "react/jsx-runtime" export 시 타입만 export하도록 수정

--- src/misc/opengraph/jsx/jsx-runtime.ts
+++ src/misc/opengraph/jsx/jsx-runtime.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-export * from "react/jsx-runtime";
+export type * from "react/jsx-runtime";
 
 export const jsx = (type: any, props: any, key: any) => ({ type, props, key });
 export const jsxs = jsx;
