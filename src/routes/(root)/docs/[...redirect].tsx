f0678c8c7eb8d1de1f686a4d1dd50978cc4db1b1
BaekGeunYoung <dvmflstm@naver.com>
2024-08-26 18:52:43 +0900

    feat: add fallback config

---
+++ src/routes/(root)/docs/[...redirect].tsx
@@ -0,0 +1,29 @@
+import {NotFoundError} from "~/components/404.tsx";
+import {cache, createAsync, Navigate, useParams} from "@solidjs/router";
+
+const loadRedirection = cache(async (slug: string) => {
+  "use server";
+
+  const { default: redirYaml } = await import(
+    "~/routes/(root)/docs/_redir.yaml"
+    );
+  const redir = redirYaml.find(({ old }) => old === slug);
+  if (!redir) return;
+  return redir.new
+}, "docs/redirection");
+
+export default function Redirect() {
+  const params = useParams();
+  const redirection = createAsync(async () => {
+    const slug = params.redirect
+    if (!slug) return;
+    return await loadRedirection(`/${slug}`);
+  });
+
+  const destination = redirection()
+
+  if (!destination) throw new NotFoundError();
+  else return <Navigate href={destination} />
+
+
+}
--- src/routes/(root)/index.tsx
+++ src/routes/(root)/index.tsx
@@ -1,4 +1,4 @@
-import { Navigate } from "@solidjs/router";
+import {Navigate} from "@solidjs/router";
 
 export default function Index() {
   return <Navigate href="/opi/ko/readme" />;
