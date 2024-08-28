410546c85f566605e11ba577cf288515d6706c5c
Ashley <ashley@chai.finance>
2024-08-28 16:08:15 +0900

    url 변경

---
+++ src/components/EasyGuideLink.tsx
@@ -0,0 +1,14 @@
+export default function EasyGuideLink() {
+  return (
+    <div>
+      <a
+        class="my-4 inline-flex items-center gap-2 rounded bg-orange-600 px-5 py-2 text-lg text-orange-1 font-bold"
+        target="_blank"
+        href="https://www.canva.com/design/DAFqFrw4MPs/clixBNoIORAzdqJB0SlOoA/view?utm_content=DA[…]MPs&utm_campaign=designshare&utm_medium=link&utm_source=editor"
+      >
+        <span>포트원 서비스 간편 가이드</span>
+        <i class="i-ic-baseline-launch" />
+      </a>
+    </div>
+  );
+}
