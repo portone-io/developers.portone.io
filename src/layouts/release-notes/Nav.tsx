d4428620c38e828d0e1b08bc56e454a9c1f2c4f4
CirnoV (Sickle) <cirnov59@gmail.com>
2024-09-03 17:21:33 +0900

    릴리즈 노트 activeSlug 버그 수정 (#606)

--- src/layouts/release-notes/Nav.tsx
+++ src/layouts/release-notes/Nav.tsx
@@ -1,3 +1,6 @@
+import { useLocation } from "@solidjs/router";
+import { createMemo } from "solid-js";
+
 import type { ReleaseNote } from "~/misc/releaseNote";
 
 import NoteList from "./NoteList";
@@ -6,27 +9,31 @@ interface Props {
   consoleNotes: ReleaseNote[];
   apiSdkNotes: ReleaseNote[];
   platformNotes: ReleaseNote[];
-  activeSlug?: string;
 }
 
 export default function Nav(props: Props) {
+  const location = useLocation();
+  const activeSlug = createMemo(() =>
+    location.pathname.replace(/^\/release-notes\//, ""),
+  );
+
   return (
     <nav>
       <ul class="flex flex-col gap-1 p-2 py-4">
         <NoteList
           notes={props.consoleNotes}
           title="관리자콘솔"
-          activeSlug={props.activeSlug}
+          activeSlug={activeSlug()}
         />
         <NoteList
           notes={props.apiSdkNotes}
           title="API / SDK"
-          activeSlug={props.activeSlug}
+          activeSlug={activeSlug()}
         />
         <NoteList
           notes={props.platformNotes}
           title="파트너 정산 자동화"
-          activeSlug={props.activeSlug}
+          activeSlug={activeSlug()}
         />
       </ul>
     </nav>
