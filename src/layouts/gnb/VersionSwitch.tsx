02b8e7c7ce307c14ac349e9cfa8ad39fb0a7552f
CirnoV <cirnov59@gmail.com>
2024-09-25 20:29:44 +0900

    버전 스위치 버그 수정

--- src/layouts/gnb/VersionSwitch.tsx
+++ src/layouts/gnb/VersionSwitch.tsx
@@ -1,6 +1,12 @@
 import { useLocation, useNavigate } from "@solidjs/router";
 import clsx from "clsx";
-import { createEffect, createSignal, startTransition } from "solid-js";
+import {
+  createEffect,
+  createMemo,
+  createSignal,
+  Show,
+  startTransition,
+} from "solid-js";
 
 import type { DocsEntry } from "~/content/config";
 import { useSystemVersion } from "~/state/system-version";
@@ -23,9 +29,9 @@ export function VersionSwitch(props: VersionSwitchProps) {
 
   const location = useLocation();
   const navigate = useNavigate();
-
-  if (hiddenPaths.some((path) => location.pathname.startsWith(path)))
-    return null;
+  const show = createMemo(
+    () => !hiddenPaths.some((path) => location.pathname.startsWith(path)),
+  );
 
   const { systemVersion, setSystemVersion } = useSystemVersion();
 
@@ -68,50 +74,51 @@ export function VersionSwitch(props: VersionSwitchProps) {
   }, [showPopover]);
 
   return (
-    <div class="relative">
-      <div
-        style={{ transition: "margin 0.1s" }}
-        onClick={() => {
-          const newVersion = systemVersion() !== "v1" ? "v1" : "v2";
-          void startTransition(() => {
-            setSystemVersion(newVersion);
-          });
-          setShowPopover(false);
-
-          const mappedPath =
-            Object.entries(pathMappings).find(([from]) =>
-              location.pathname.startsWith(from),
-            )?.[1] ??
-            (props.docData?.versionVariants?.[newVersion] &&
-              `/opi${props.docData.versionVariants[newVersion]}`);
-          if (mappedPath) navigate(mappedPath);
-          else if (location.pathname.startsWith("/opi/")) return;
-          else navigate("/");
-        }}
-        class={clsx(
-          "bg-slate-1 border-slate-3 text-12px text-slate-5 p-1px border-1 flex cursor-pointer select-none overflow-hidden whitespace-pre rounded-[6px] text-center font-bold",
-          props.class,
-        )}
-      >
-        <div class={getVersionClass("v1", systemVersion())}>V1</div>
-        <div class={getVersionClass("v2", systemVersion())}>V2</div>
-      </div>
-      {(props.docData?.versionVariants ||
-        (props.docData?.targetVersions?.length ?? 0) > 1) && (
+    <Show when={show()}>
+      <div class="relative">
         <div
-          ref={popoverRef}
-          class="absolute inset-x-0 top-[calc(100%+16px)] opacity-0"
+          style={{ transition: "margin 0.1s" }}
+          onClick={() => {
+            const newVersion = systemVersion() !== "v1" ? "v1" : "v2";
+            void startTransition(() => {
+              setSystemVersion(newVersion);
+            });
+            setShowPopover(false);
+
+            const mappedPath =
+              Object.entries(pathMappings).find(([from]) =>
+                location.pathname.startsWith(from),
+              )?.[1] ??
+              (props.docData?.versionVariants?.[newVersion] &&
+                `/opi${props.docData.versionVariants[newVersion]}`);
+            if (mappedPath) navigate(mappedPath);
+            else if (location.pathname.startsWith("/opi/")) return;
+            else navigate("/");
+          }}
+          class={clsx(
+            "bg-slate-1 border-slate-3 text-12px text-slate-5 p-1px border-1 flex cursor-pointer select-none overflow-hidden whitespace-pre rounded-[6px] text-center font-bold",
+            props.class,
+          )}
         >
-          <div class="absolute left-1/2 top-0 whitespace-nowrap rounded bg-portone px-2 py-1 text-white font-semibold shadow-md -translate-x-1/2">
-            이 페이지의 다른 버전 보기
-            <div class="absolute left-1/2 rotate-45 border-10px border-transparent border-l-portone border-t-portone -top-8px -translate-x-1/2" />
-          </div>
+          <div class={getVersionClass("v1", systemVersion())}>V1</div>
+          <div class={getVersionClass("v2", systemVersion())}>V2</div>
         </div>
-      )}
-    </div>
+        {(props.docData?.versionVariants ||
+          (props.docData?.targetVersions?.length ?? 0) > 1) && (
+          <div
+            ref={popoverRef}
+            class="absolute inset-x-0 top-[calc(100%+16px)] opacity-0"
+          >
+            <div class="absolute left-1/2 top-0 whitespace-nowrap rounded bg-portone px-2 py-1 text-white font-semibold shadow-md -translate-x-1/2">
+              이 페이지의 다른 버전 보기
+              <div class="absolute left-1/2 rotate-45 border-10px border-transparent border-l-portone border-t-portone -top-8px -translate-x-1/2" />
+            </div>
+          </div>
+        )}
+      </div>
+    </Show>
   );
 }
-export default VersionSwitch;
 
 function getVersionClass(
   thisVersion: SystemVersion,
