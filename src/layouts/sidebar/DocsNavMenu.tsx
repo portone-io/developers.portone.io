080799548df51518a9b487c23cff5cd38eea6e79
CirnoV <cirnov59@gmail.com>
2024-10-28 01:29:07 +0900

    파트너 정산 자동화 이동 링크 변경

--- src/layouts/sidebar/DocsNavMenu.tsx
+++ src/layouts/sidebar/DocsNavMenu.tsx
@@ -39,7 +39,7 @@ export default function DocsNavMenu(props: Props) {
               { label: "원 페이먼트 인프라", link: "/opi/ko" },
               { label: "REST API", link: "/api/rest-v2" },
               { label: "포트원 SDK", link: "/sdk/ko" },
-              { label: "파트너 정산 자동화", link: "/platform" },
+              { label: "파트너 정산 자동화", link: "/platform/ko" },
               { label: "릴리스 노트", link: "/release-notes" },
               { label: "기술 블로그", link: "/blog" },
             ]}
