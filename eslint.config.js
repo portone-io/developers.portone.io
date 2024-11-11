075096665c4e1ad028ecab14e318ab1c8ea8e918
CirnoV <cirnov59@gmail.com>
2024-11-11 19:10:24 +0900

    잘못된 eslint 설정 수정

--- eslint.config.js
+++ eslint.config.js
@@ -40,7 +40,7 @@ const tsTypeCheckedRules = {
 
 /** @type {import("eslint").Linter.FlatConfig[]} */
 export default [
-  { ignores: ["dist", ".vercel", "**/docs/**/*"] },
+  { ignores: ["dist", ".vercel", "**/routes/(root)/docs/**/*"] },
   {
     ...eslint.configs.recommended,
     ignores: ["scripts/**/*.ts", "**/*.mdx/*"],
