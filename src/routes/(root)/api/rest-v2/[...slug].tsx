a81347cf4eaa522d4aead37166f218182eb65f39
doohochang <doohochang@gmail.com>
2024-11-12 17:14:12 +0900

    feat: add description about V2 API backward-compatibility

--- src/routes/(root)/api/rest-v2/[...slug].tsx
+++ src/routes/(root)/api/rest-v2/[...slug].tsx
@@ -61,12 +61,7 @@ export default function ApiV2Docs() {
           </SchemaDownloadButton>
         </prose.p>
         <prose.h3>요청 및 응답 형식</prose.h3>
-        <prose.p>
-          요청과 응답의 본문은 JSON 형식입니다.
-          <br />
-          API 응답에 포함된 필드는 별도 안내 없이 추가될 수 있으니, 알지 못하는
-          필드가 있는 경우에는 무시하도록 개발해 주세요.
-        </prose.p>
+        <prose.p>요청과 응답의 본문은 JSON 형식입니다.</prose.p>
         <prose.p>
           API 매개 변수 중 URL 경로에 들어가는 문자열 값이 있는 경우, URL 경로에
           들어갈 수 없는 문자열은 이스케이프하여야 합니다. 자바스크립트의{" "}
@@ -75,35 +70,85 @@ export default function ApiV2Docs() {
         </prose.p>
         <prose.h3 id="auth">인증 방식</prose.h3>
         <prose.p>
-          V2 API를 사용하기 위해서는 V2 API Secret이 필요하며, 포트원 콘솔 내
-          결제연동 탭에서 발급받을 수 있습니다.
+          V2 API 를 사용하기 위해서는 V2 API Secret 이 필요하며, 포트원
+          관리자콘솔 내 결제연동 탭에서 발급받으실 수 있습니다.
         </prose.p>
         <prose.p>
-          인증 관련 API를 제외한 모든 API는 HTTP <code>Authorization</code>{" "}
-          헤더로 인증 정보를 전달해 주셔야 합니다. Authorization 헤더에 전달하는
-          형식은 두 가지 중 하나입니다.
+          인증 관련 API 를 제외한 모든 API 는 HTTP <code>Authorization</code>{" "}
+          헤더로 아래 형식의 인증 정보를 전달해주셔야 합니다.
         </prose.p>
         <ul>
           <li>
-            API Secret 직접 사용 (간편)
-            <br />
-            Authorization: PortOne <i>MY_API_SECRET</i>
-          </li>
-          <li>
-            액세스 토큰 사용
-            <br />
-            Authorization: Bearer <i>MY_ACCESS_TOKEN</i>
+            <code>
+              Authorization: PortOne <i>MY_API_SECRET</i>
+            </code>
           </li>
         </ul>
-        액세스 토큰을 사용한 인증을 원하는 경우, 인증 관련 API를 이용해 주세요.
-        <prose.h3 id="get-with-body">GET 요청 시 Body 대신 Query 사용</prose.h3>
+        <prose.h3 id="get-with-body">
+          GET 요청 시 Body 대신 Query 사용하기
+        </prose.h3>
         <prose.p>
-          GET 요청 시에 Body를 사용해야 하는 경우, Body 대신 Query를 사용할 수
+          GET 요청 시에 Body 를 전달해야 하는 경우, Body 대신 Query 를 사용할 수
           있습니다.
         </prose.p>
         <prose.p>
           이 경우, Body 객체를 <code>requestBody</code> Query 필드에 넣어주시면
           됩니다.
+        </prose.p>
+        <prose.h3 id="backward-compatibility">하위호환성</prose.h3>
+        <prose.p>
+          포트원이 제공하는 모든 Stable API 에 대해 아래와 같은 하위호환성이
+          보장됩니다.
+        </prose.p>
+        <ol>
+          <li>
+            <prose.p>
+              현재 사용 가능한 입력 형식은 앞으로도 사용할 수 있습니다.
+            </prose.p>
+            <ul>
+              <li> 입력 형식 내 필드 정의가 삭제되지 않습니다. </li>
+              <li>
+                <prose.p>
+                  필수 입력 정보가 추가되거나, 선택 입력 정보가 필수로 변경되지
+                  않습니다.
+                </prose.p>
+                <ul>
+                  <li> 오로지 선택 입력 정보만 추가될 수 있습니다. </li>
+                </ul>
+              </li>
+              <li>하위 필드의 형식(타입) 또한 위 규칙을 지키며 변경됩니다.</li>
+              <li> enum 타입의 값이 삭제되지 않습니다. </li>
+            </ul>
+          </li>
+          <li>
+            <prose.p>출력 형식이 확장될 수 있지만, 축소되지 않습니다.</prose.p>
+            <ul>
+              <li> 출력 형식 내 필드 정의가 삭제되지 않습니다. </li>
+              <li>
+                사용 중인 필수 출력 정보가 선택사항으로 변경되거나 출력 시
+                누락되지 않습니다.
+              </li>
+              <li>하위 필드의 형식(타입) 또한 위 규칙을 지키며 변경됩니다.</li>
+              <li>
+                <prose.p>
+                  단,{" "}
+                  <strong>
+                    새로운 필드 또는 enum 값, oneOf 케이스가 추가될 수 있습니다.
+                  </strong>
+                </prose.p>
+                <ul>
+                  <li>
+                    알지 못하는 필드 및 값이 주어지더라도 crash 가 발생하지
+                    않도록 유의하여 개발해주세요.
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ol>
+        <prose.p>
+          또한 <code>UNSTABLE</code> 이 표기된 일부 API 의 경우, 위 하위호환성
+          정책과 무관하게 변경 및 지원 종료될 수 있으니 이용에 유의하세요.
         </prose.p>
       </RestApi>
     </div>
