19c60fa72d105f8e40c76ebab4d123f775fb3586
CirnoV <cirnov59@gmail.com>
2024-11-13 21:02:16 +0900

    타입 오류 수정

--- src/routes/(root)/opi/ko/quick-guide/_preview/frontend/react/index.ts
+++ src/routes/(root)/opi/ko/quick-guide/_preview/frontend/react/index.ts
@@ -1,4 +1,4 @@
-import type { CodeExample } from "~/components/interactive-docs/index.jsx";
+import type { CodeExample } from "~/state/interactive-docs/index.jsx";
 
 import type { Params, Sections } from "../../type.js";
 import app from "./app.jsx";
