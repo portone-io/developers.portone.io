7b44969c7e423b8d22f56959c17955ae9a55c69c
CirnoV (Sickle) <cirnov59@gmail.com>
2024-11-13 12:02:34 +0900

    WIP on feat/quick-guide

---
+++ src/components/interactive-docs/CodeTabs.tsx
@@ -0,0 +1,40 @@
+import { Tabs } from "@kobalte/core/tabs";
+import clsx from "clsx";
+import { For } from "solid-js";
+
+import { CodeIcon } from "./CodeIcon";
+
+interface CodeTabsProps {
+  tabs: string[];
+  selectedTab: string;
+  onChange: (tab: string) => void;
+}
+
+export function CodeTabs(props: CodeTabsProps) {
+  return (
+    <Tabs
+      value={props.selectedTab}
+      onChange={props.onChange}
+      class="w-full flex overflow-x-hidden"
+    >
+      <Tabs.List class="flex flex-nowrap gap-2 overflow-x-scroll whitespace-nowrap">
+        <For each={props.tabs}>
+          {(tab) => (
+            <Tabs.Trigger
+              class={clsx(
+                "flex flex-1 items-center gap-1 rounded-md px-3 py-1",
+                tab === props.selectedTab
+                  ? "bg-slate-2 text-slate-9"
+                  : "hover:bg-slate-5 text-slate-4 hover:text-slate-3",
+              )}
+              value={tab}
+            >
+              <CodeIcon fileName={tab} />
+              <span class={clsx("text-xs font-medium leading-6")}>{tab}</span>
+            </Tabs.Trigger>
+          )}
+        </For>
+      </Tabs.List>
+    </Tabs>
+  );
+}
--- src/components/interactive-docs/CodeViewer.tsx
+++ src/components/interactive-docs/CodeViewer.tsx
@@ -1,9 +1,24 @@
+import { createSignal } from "solid-js";
+
+import { CodeTabs } from "./CodeTabs";
+
 interface CodeViewerProps {}
 
 export default function CodeViewer(props: CodeViewerProps) {
+  const tabs = ["server.js", "App.jsx"];
+  const [selectedTab, setSelectedTab] = createSignal(tabs[0]!);
   return (
-    <div class="grid grid-rows-[min-content_1fr] gap-y-2 p-2">
-      <div></div>
+    <div class="grid grid-rows-[min-content_1fr] gap-y-2 rounded-t-xl bg-slate-8 p-2">
+      <div class="grid grid-cols-[1fr_min-content] h-12 items-center gap-2 rounded-lg bg-slate-7 p-2">
+        <CodeTabs
+          tabs={tabs}
+          selectedTab={selectedTab()}
+          onChange={setSelectedTab}
+        />
+        <button class="px-2 text-slate-5" type="button">
+          <i class="i-material-symbols-content-copy-outline inline-block text-xl" />
+        </button>
+      </div>
     </div>
   );
 }
