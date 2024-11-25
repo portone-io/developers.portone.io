f0ed8ae4cb08c907d15a427d53ed917ac0cf7094
CirnoV <cirnov59@gmail.com>
2024-11-26 01:27:23 +0900

    CodeRenderer 제외 preload 구현

--- src/routes/(root)/opi/ko/quick-guide/payment/_preview/frontend/react/index.ts
+++ src/routes/(root)/opi/ko/quick-guide/payment/_preview/frontend/react/index.ts
@@ -1,7 +1,7 @@
 import type { CodeExample } from "~/state/interactive-docs/index.jsx";
 
 import type { Params, Sections } from "../../type.js";
-import app from "./app.jsx";
+import app from "./app.jsx.js";
 
 export const files = [
   { fileName: "app.jsx", code: app },
