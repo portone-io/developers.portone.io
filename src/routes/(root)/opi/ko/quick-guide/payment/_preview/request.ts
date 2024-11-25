6838df52cd4c1b6930bfd430957dd8d3c55fd7f0
kiwiyou <kiwiyou@portone.io>
2024-11-25 16:04:09 +0900

    feat: add api secret docs

--- src/routes/(root)/opi/ko/quick-guide/payment/_preview/request.ts
+++ src/routes/(root)/opi/ko/quick-guide/payment/_preview/request.ts
@@ -16,11 +16,106 @@ const virtualAccountPayment = {
   },
 } satisfies Partial<PortOne.PaymentRequest>;
 
+const customer = {
+  fullName: "포트원",
+  email: "example@portone.io",
+  phoneNumber: "01012341234",
+} satisfies PortOne.Entity.Customer;
+
+const overrides = {
+  toss: {
+    card: {
+      channelKey: "channel-key-ebe7daa6-4fe4-41bd-b17d-3495264399b5",
+      ...cardPayment,
+    },
+    virtualAccount: {
+      channelKey: "channel-key-ebe7daa6-4fe4-41bd-b17d-3495264399b5",
+      ...virtualAccountPayment,
+    },
+  },
+  nice: {
+    card: {
+      channelKey: "channel-key-4ca6a942-3ee0-48fb-93ef-f4294b876d28",
+      ...cardPayment,
+    },
+    virtualAccount: {
+      channelKey: "channel-key-e6c31df1-5559-4b4a-9b2c-a35793d14db2",
+      ...virtualAccountPayment,
+    },
+  },
+  smartro: {
+    card: {
+      channelKey: "channel-key-c4a4b281-a1e5-40c9-8140-f055262bcefd",
+      ...cardPayment,
+      customer: {
+        phoneNumber: customer.phoneNumber,
+      },
+    },
+    virtualAccount: {
+      channelKey: "channel-key-c4a4b281-a1e5-40c9-8140-f055262bcefd",
+      ...virtualAccountPayment,
+      customer: {
+        phoneNumber: customer.phoneNumber,
+      },
+    },
+  },
+  inicis: {
+    card: {
+      channelKey: "channel-key-fc5f33bb-c51e-4ac7-a0df-4dc40330046d",
+      ...cardPayment,
+      customer,
+    },
+    virtualAccount: {
+      channelKey: "channel-key-fc5f33bb-c51e-4ac7-a0df-4dc40330046d",
+      ...virtualAccountPayment,
+      customer,
+    },
+  },
+  kcp: {
+    card: {
+      channelKey: "channel-key-a79920e0-a898-49f0-aab7-50aa6834848f",
+      ...cardPayment,
+    },
+    virtualAccount: {
+      channelKey: "channel-key-a79920e0-a898-49f0-aab7-50aa6834848f",
+      ...virtualAccountPayment,
+    },
+  },
+  kpn: {
+    card: {
+      channelKey: "channel-key-bcbb1622-ff80-49d5-adef-49191fda8ede",
+      ...cardPayment,
+    },
+    virtualAccount: {
+      channelKey: "channel-key-bcbb1622-ff80-49d5-adef-49191fda8ede",
+      ...virtualAccountPayment,
+    },
+  },
+  ksnet: {
+    card: {
+      channelKey: "channel-key-4a5daa34-aecb-44af-aaad-e42384acfb6e",
+      ...cardPayment,
+      customer: {
+        fullName: customer.fullName,
+      },
+    },
+    virtualAccount: {
+      channelKey: "channel-key-4a5daa34-aecb-44af-aaad-e42384acfb6e",
+      ...virtualAccountPayment,
+      customer: {
+        fullName: "포트원",
+      },
+    },
+  },
+} as const;
+
 function templatedPayment(
+  paymentId: string,
   overrides: Partial<PortOne.PaymentRequest> &
-    Pick<PortOne.PaymentRequest, "channelKey" | "payMethod" | "paymentId">,
+    Pick<PortOne.PaymentRequest, "channelKey" | "payMethod">,
 ): PortOne.PaymentRequest {
   return {
+    paymentId,
     storeId: "store-e4038486-8d83-41a5-acf1-844a009e0d94",
     orderName: "신발",
     totalAmount: 1000,
@@ -33,124 +128,46 @@ function templatedPayment(
 export function createPaymentRequest(params: Params, paymentId: string) {
   return match(params)
     .with({ pg: { name: "toss", payMethods: "card" } }, () =>
-      templatedPayment({
-        paymentId,
-        channelKey: "channel-key-ebe7daa6-4fe4-41bd-b17d-3495264399b5",
-        ...cardPayment,
-      }),
+      templatedPayment(paymentId, overrides.toss.card),
     )
     .with({ pg: { name: "toss", payMethods: "virtualAccount" } }, () =>
-      templatedPayment({
-        paymentId,
-        channelKey: "channel-key-ebe7daa6-4fe4-41bd-b17d-3495264399b5",
-        ...virtualAccountPayment,
-      }),
+      templatedPayment(paymentId, overrides.toss.virtualAccount),
     )
     .with({ pg: { name: "nice", payMethods: "card" } }, () =>
-      templatedPayment({
-        paymentId,
-        channelKey: "channel-key-4ca6a942-3ee0-48fb-93ef-f4294b876d28",
-        ...cardPayment,
-      }),
+      templatedPayment(paymentId, overrides.nice.card),
     )
     .with({ pg: { name: "nice", payMethods: "virtualAccount" } }, () =>
-      templatedPayment({
-        paymentId,
-        channelKey: "channel-key-e6c31df1-5559-4b4a-9b2c-a35793d14db2",
-        ...virtualAccountPayment,
-      }),
+      templatedPayment(paymentId, overrides.nice.virtualAccount),
     )
     .with({ pg: { name: "smartro", payMethods: "card" } }, () =>
-      templatedPayment({
-        paymentId,
-        channelKey: "channel-key-c4a4b281-a1e5-40c9-8140-f055262bcefd",
-        ...cardPayment,
-        customer: {
-          phoneNumber: "01012341234",
-        },
-      }),
+      templatedPayment(paymentId, overrides.smartro.card),
     )
     .with({ pg: { name: "smartro", payMethods: "virtualAccount" } }, () =>
-      templatedPayment({
-        paymentId,
-        channelKey: "channel-key-c4a4b281-a1e5-40c9-8140-f055262bcefd",
-        ...virtualAccountPayment,
-        customer: {
-          phoneNumber: "01012341234",
-        },
-      }),
+      templatedPayment(paymentId, overrides.smartro.virtualAccount),
     )
     .with({ pg: { name: "inicis", payMethods: "card" } }, () =>
-      templatedPayment({
-        paymentId,
-        channelKey: "channel-key-fc5f33bb-c51e-4ac7-a0df-4dc40330046d",
-        ...cardPayment,
-        customer: {
-          fullName: "포트원",
-          email: "example@portone.io",
-          phoneNumber: "01012341234",
-        },
-      }),
+      templatedPayment(paymentId, overrides.inicis.card),
     )
     .with({ pg: { name: "inicis", payMethods: "virtualAccount" } }, () =>
-      templatedPayment({
-        paymentId,
-        channelKey: "channel-key-fc5f33bb-c51e-4ac7-a0df-4dc40330046d",
-        ...virtualAccountPayment,
-        customer: {
-          fullName: "포트원",
-          email: "example@portone.io",
-          phoneNumber: "01012341234",
-        },
-      }),
+      templatedPayment(paymentId, overrides.inicis.virtualAccount),
     )
     .with({ pg: { name: "kcp", payMethods: "card" } }, () =>
-      templatedPayment({
-        paymentId,
-        channelKey: "channel-key-a79920e0-a898-49f0-aab7-50aa6834848f",
-        ...cardPayment,
-      }),
+      templatedPayment(paymentId, overrides.kcp.card),
     )
     .with({ pg: { name: "kcp", payMethods: "virtualAccount" } }, () =>
-      templatedPayment({
-        paymentId,
-        channelKey: "channel-key-a79920e0-a898-49f0-aab7-50aa6834848f",
-        ...virtualAccountPayment,
-      }),
+      templatedPayment(paymentId, overrides.kcp.virtualAccount),
     )
     .with({ pg: { name: "kpn", payMethods: "card" } }, () =>
-      templatedPayment({
-        paymentId,
-        channelKey: "channel-key-bcbb1622-ff80-49d5-adef-49191fda8ede",
-        ...cardPayment,
-      }),
+      templatedPayment(paymentId, overrides.kpn.card),
     )
     .with({ pg: { name: "kpn", payMethods: "virtualAccount" } }, () =>
-      templatedPayment({
-        paymentId,
-        channelKey: "channel-key-bcbb1622-ff80-49d5-adef-49191fda8ede",
-        ...virtualAccountPayment,
-      }),
+      templatedPayment(paymentId, overrides.kpn.virtualAccount),
     )
     .with({ pg: { name: "ksnet", payMethods: "card" } }, () =>
-      templatedPayment({
-        paymentId,
-        channelKey: "channel-key-4a5daa34-aecb-44af-aaad-e42384acfb6e",
-        ...cardPayment,
-        customer: {
-          fullName: "포트원",
-        },
-      }),
+      templatedPayment(paymentId, overrides.ksnet.card),
     )
     .with({ pg: { name: "ksnet", payMethods: "virtualAccount" } }, () =>
-      templatedPayment({
-        paymentId,
-        channelKey: "channel-key-4a5daa34-aecb-44af-aaad-e42384acfb6e",
-        ...virtualAccountPayment,
-        customer: {
-          fullName: "포트원",
-        },
-      }),
+      templatedPayment(paymentId, overrides.ksnet.virtualAccount),
     )
     .exhaustive();
 }
