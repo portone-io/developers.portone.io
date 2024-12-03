d55f4de3146719ef73165d9ed9e9017584e4f429
CirnoV (Sickle) <cirnov59@gmail.com>
2024-12-03 21:18:31 +0900

    퀵 가이드 상태 정리
    - Client Side Routing으로 퀵 가이드 페이지로 돌아올 때 발생하는 오류 수정
    - [on](https://docs.solidjs.com/reference/reactive-utilities/on-util) 메소드로 명시적인 reactive 구현

--- src/state/interactive-docs/index.tsx
+++ src/state/interactive-docs/index.tsx
@@ -2,6 +2,7 @@ import { createContextProvider } from "@solid-primitives/context";
 import { createHighlighterCore } from "shiki/core";
 import { createOnigurumaEngine } from "shiki/engine/oniguruma";
 import {
+  batch,
   type Component,
   createEffect,
   createMemo,
@@ -88,49 +89,87 @@ export type InteractiveDocsInit = {
 
 const [InteractiveDocsProvider, useInteractiveDocs] = createContextProvider(
   (props: { initial?: InteractiveDocsInit }) => {
-    const initial = createMemo<InteractiveDocsInit>(
-      () =>
-        props.initial ??
-        ({
-          pgOptions: {
-            hyphen: { payMethods: ["card"] },
-          },
-          languages: {
-            frontend: ["react"],
-            backend: ["node"],
-            hybrid: ["nextjs"],
-          },
-          params: {
-            pg: {
-              name: "hyphen",
-              payMethods: "card",
-            },
-          },
-        } satisfies InteractiveDocsInit),
-    );
+    const defaultInitial: InteractiveDocsInit = {
+      pgOptions: {
+        hyphen: { payMethods: ["card"] },
+      },
+      languages: {
+        frontend: ["react"],
+        backend: ["node"],
+        hybrid: ["nextjs"],
+      },
+      params: {
+        pg: {
+          name: "hyphen",
+          payMethods: "card",
+        },
+      },
+    };
+    const initial = createMemo(() => props.initial ?? defaultInitial);
     const [params, setParams] = createStore<Params>(initial().params);
+    // initial 변경 시 DefaultParams 값 초기화
+    createEffect(
+      on(initial, (initial) => {
+        setParams(initial.params);
+      }),
+    );
+
     const [preview, setPreview] = createSignal<Component | undefined>(
       undefined,
     );
     const pgName = createMemo(() => params.pg.name);
     const pgOptions = createMemo(() => initial().pgOptions);
-    createEffect(() => {
-      const pg = Object.keys(pgOptions())[0] as Pg | undefined;
-      if (!pg) return;
-      const pgOption = pgOptions()[pg];
-      if (!pgOption) return;
-      setParams("pg", {
-        name: pg,
-        payMethods: pgOption.payMethods[0],
-      });
-    });
+    // PG사 변경 시 처리
+    createEffect(
+      on([pgName, pgOptions], ([pgName, pgOptions]) => {
+        const pgOption = pgOptions[pgName];
+        if (pgOption === undefined) {
+          const firstPgName = Object.keys(pgOptions)[0];
+          if (firstPgName) {
+            setParams("pg", "name", firstPgName as Pg);
+          }
+        } else {
+          const payMethod = pgOption.payMethods.find(
+            (method) => method === params.pg.payMethods,
+          );
+          if (!payMethod && pgOption.payMethods[0]) {
+            setParams("pg", "payMethods", pgOption.payMethods[0]);
+          }
+        }
+      }),
+    );
+
     const languages = createMemo(() => initial().languages);
     const [selectedLanguage, setSelectedLanguage] = createSignal<
-      [frontend: string, backend: string] | string
-    >([initial().languages.frontend[0], initial().languages.backend[0]]);
-    createEffect(() => {
-      setSelectedLanguage([languages().frontend[0], languages().backend[0]]);
-    });
+      [frontend: string, backend: string] | string | null
+    >([languages().frontend[0], languages().backend[0]]);
+    // 언어 변경 시 처리
+    createEffect(
+      on([selectedLanguage, languages], ([selectedLanguage, languages]) => {
+        if (selectedLanguage === null) return;
+        if (Array.isArray(selectedLanguage)) {
+          const [frontend, backend] = selectedLanguage;
+          batch(() => {
+            if (!languages.frontend.includes(frontend)) {
+              setSelectedLanguage([languages.frontend[0], backend]);
+            }
+            if (!languages.backend.includes(backend)) {
+              setSelectedLanguage([frontend, languages.backend[0]]);
+            }
+          });
+        } else {
+          if (!languages.hybrid.includes(selectedLanguage)) {
+            setSelectedLanguage(
+              languages.hybrid[0] ?? [
+                languages.frontend[0],
+                languages.backend[0],
+              ],
+            );
+          }
+        }
+      }),
+    );
+
     const [codeExamples, setCodeExamples] = createSignal<{
       frontend: Record<string, CodeExample<Params, string>[]>;
       backend: Record<string, CodeExample<Params, string>[]>;
@@ -140,33 +179,45 @@ const [InteractiveDocsProvider, useInteractiveDocs] = createContextProvider(
       backend: {},
     });
     const [tabs, setTabs] = createSignal<Tab[]>([]);
-    createEffect(() => {
-      const resolveCode = (example: CodeExample<Params, string>): Tab => {
-        const { code, sections } = example.code(params);
-        return {
-          fileName: example.fileName,
-          code,
-          sections: sections as Record<string, Section>,
-        };
-      };
-      const _selectedLanguage = selectedLanguage();
-      if (_selectedLanguage === null) return [];
-      if (Array.isArray(_selectedLanguage)) {
-        const [frontend, backend] = _selectedLanguage;
-        return setTabs(
-          [
-            ...(codeExamples()["frontend"][frontend] ?? []),
-            ...(codeExamples()["backend"][backend] ?? []),
-          ].map(resolveCode),
-        );
-      }
-      const hybrid = codeExamples()["hybrid"];
-      return setTabs(
-        hybrid ? (hybrid[_selectedLanguage] ?? []).map(resolveCode) : [],
-      );
-    });
+    createEffect(
+      on(
+        [selectedLanguage, codeExamples],
+        ([selectedLanguage, codeExamples]) => {
+          const resolveCode = (example: CodeExample<Params, string>): Tab => {
+            const { code, sections } = example.code(params);
+            return {
+              fileName: example.fileName,
+              code,
+              sections: sections as Record<string, Section>,
+            };
+          };
+          if (selectedLanguage === null) return [];
+          if (Array.isArray(selectedLanguage)) {
+            const [frontend, backend] = selectedLanguage;
+            return setTabs(
+              [
+                ...(codeExamples["frontend"][frontend] ?? []),
+                ...(codeExamples["backend"][backend] ?? []),
+              ].map(resolveCode),
+            );
+          }
+          const hybrid = codeExamples["hybrid"];
+          return setTabs(
+            hybrid ? (hybrid[selectedLanguage] ?? []).map(resolveCode) : [],
+          );
+        },
+      ),
+    );
     const [selectedTab, setSelectedTab] = createSignal<string | null>(
       tabs()[0]?.fileName ?? null,
+    );
+    createEffect(
+      on([tabs, selectedTab], ([tabs, selectedTab]) => {
+        const tab = tabs.find((tab) => tab.fileName === selectedTab);
+        if (!tab) {
+          setSelectedTab(tabs[0]?.fileName ?? null);
+        }
+      }),
     );
     const sections = createMemo<Record<string, { fileName: string } & Section>>(
       () => {
@@ -204,34 +255,20 @@ const [InteractiveDocsProvider, useInteractiveDocs] = createContextProvider(
       if (!section) return null;
       return sections()[section] ?? null;
     });
-
     // Section 변경 시 Tab 이동
-    createEffect(() => {
-      const section = highlightSection();
-      if (!section) return;
-      const tab = tabs().find((tab) => tab.fileName === section.fileName);
-      if (tab) {
-        setSelectedTab(tab.fileName);
-      }
-    });
-    const [highlighter, setHighlighter] =
-      createSignal<Awaited<ReturnType<typeof createHighlighterCore>>>();
-    void highlighterInstance.then(setHighlighter);
-
-    // PG사 변경 시 payMethods 초기화
     createEffect(
-      on([pgName, pgOptions], ([pgName, pgOptions]) => {
-        const pgOption = pgOptions[pgName];
-        if (!pgOption) return;
-
-        const payMethod = pgOption.payMethods.find(
-          (method) => method === params.pg.payMethods,
-        );
-        if (payMethod === undefined && pgOption.payMethods[0]) {
-          setParams("pg", "payMethods", pgOption.payMethods[0]);
+      on([highlightSection, tabs], ([section, tabs]) => {
+        if (!section) return;
+        const tab = tabs.find((tab) => tab.fileName === section.fileName);
+        if (tab) {
+          setSelectedTab(tab?.fileName);
         }
       }),
     );
+
+    const [highlighter, setHighlighter] =
+      createSignal<Awaited<ReturnType<typeof createHighlighterCore>>>();
+    void highlighterInstance.then(setHighlighter);
 
     return {
       pgOptions,
