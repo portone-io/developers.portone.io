e4d7fc6ea5488a827d840fdafe2bd99cad0936c6
CirnoV (Sickle) <cirnov59@gmail.com>
2024-12-05 14:05:35 +0900

    feat: collapsible preview

--- src/components/interactive-docs/CodePreview.tsx
+++ src/components/interactive-docs/CodePreview.tsx
@@ -1,15 +1,24 @@
+import { Collapsible } from "@kobalte/core/collapsible";
 import { Dynamic } from "solid-js/web";
 
 import { useInteractiveDocs } from "~/state/interactive-docs";
 
 export function CodePreview() {
   const { preview } = useInteractiveDocs();
+
   return (
-    <div class="grid grid-rows-[auto_auto] bg-slate-8 pt-2">
-      <div class="rounded-t-lg bg-slate-5 px-4 py-2">
+    <Collapsible
+      class="grid grid-rows-[auto_auto] bg-slate-8 pt-2"
+      forceMount
+      defaultOpen
+    >
+      <Collapsible.Trigger class="flex gap-2 rounded-t-lg bg-slate-5 px-4 py-2 [&[data-expanded]>i]:transform-rotate-180">
+        <i class="i-ic-baseline-keyboard-arrow-down text-xl text-slate-3 font-medium" />
         <span class="text-sm text-slate-3 font-medium">미리보기</span>
-      </div>
-      <Dynamic component={preview()} />
-    </div>
+      </Collapsible.Trigger>
+      <Collapsible.Content class="data-[closed]:hidden">
+        <Dynamic component={preview()} />
+      </Collapsible.Content>
+    </Collapsible>
   );
 }
