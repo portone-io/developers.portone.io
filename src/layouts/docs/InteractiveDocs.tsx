50d8b67667d8ef3e04a412332bfa8bc7a5a76dfa
CreeJee <creejee@portone.io>
2024-12-26 11:18:58 +0900

    fix(layout): v2 퀵가이드에서 사이즈가 줄어들경우 래이아웃이 찌그러지는 버그 수정 (#714)

--- src/layouts/docs/InteractiveDocs.tsx
+++ src/layouts/docs/InteractiveDocs.tsx
@@ -21,13 +21,19 @@ export function InteractiveDocs(
   const isHybridSelected = createMemo(() => !Array.isArray(selectedLanguage()));
   return (
     <div class="grid grid-cols-[1fr_1.2fr] grid-rows-[auto_1fr] flex-1 gap-y-2">
-      <div class="sticky top-26 col-span-2 min-h-12 flex items-center gap-3 from-white bg-gradient-to-r px-6">
-        <div class="rounded-md text-xs text-slate-5 font-medium">
-          결제대행사
-        </div>
-        <PgSelect />
-        <div class="rounded-md text-xs text-slate-5 font-medium">결제수단</div>
-        <PayMethodSelect />
+      <div class="sticky top-55px col-span-2 min-h-12 flex flex-wrap items-center gap-3 from-white bg-gradient-to-r px-6 z-sticky-layout md:top-26">
+        <section class="flex flex-row items-center">
+          <div class="rounded-md text-xs text-slate-5 font-medium">
+            결제대행사
+          </div>
+          <PgSelect />
+        </section>
+        <section class="flex flex-row items-center">
+          <div class="rounded-md text-xs text-slate-5 font-medium">
+            결제수단
+          </div>
+          <PayMethodSelect />
+        </section>
         <LanguageSelect languages={["frontend", "hybrid"]} title="Frontend" />
         <Show when={isHybridSelected() === false}>
           <LanguageSelect languages={"backend"} title="Backend" />
