53b2d08eb5e2b80302377603a8d1b22c29e5fccf
kiwiyou <kiwiyou@portone.io>
2025-02-24 13:13:50 +0900

    TLS 가이드 Java 6 코드 개행 (#774)

--- src/routes/(root)/opi/ko/support/_components/tls-support/java6-java-security-edit.tsx
+++ src/routes/(root)/opi/ko/support/_components/tls-support/java6-java-security-edit.tsx
@@ -13,44 +13,51 @@ export default function Java6JavaSecurityEdit() {
       <span style="font-weight: bold;background-color: lightyellow">
         # before
       </span>
-      security.provider.1=sun.security.provider.Sun
-      security.provider.2=sun.security.rsa.SunRsaSign
-      security.provider.3=com.sun.net.ssl.internal.ssl.Provider
-      security.provider.4=com.sun.crypto.provider.SunJCE
-      security.provider.5=sun.security.jgss.SunProvider
-      security.provider.6=com.sun.security.sasl.Provider
-      security.provider.7=org.jcp.xml.dsig.internal.dom.XMLDSigRI
-      security.provider.8=sun.security.smartcardio.SunPCSC
+      {"\n"}
+      security.provider.1=sun.security.provider.Sun{"\n"}
+      security.provider.2=sun.security.rsa.SunRsaSign{"\n"}
+      security.provider.3=com.sun.net.ssl.internal.ssl.Provider{"\n"}
+      security.provider.4=com.sun.crypto.provider.SunJCE{"\n"}
+      security.provider.5=sun.security.jgss.SunProvider{"\n"}
+      security.provider.6=com.sun.security.sasl.Provider{"\n"}
+      security.provider.7=org.jcp.xml.dsig.internal.dom.XMLDSigRI{"\n"}
+      security.provider.8=sun.security.smartcardio.SunPCSC{"\n"}
       <span style="font-weight: bold;background-color: lightyellow">
         # after
       </span>
+      {"\n"}
       <span style="color: gray"># Add provider with higher priority</span>
+      {"\n"}
       <span style="color:red;font-weight: bold">
         security.provider.1=org.bouncycastle.jce.provider.BouncyCastleProvider
       </span>
+      {"\n"}
       <span style="color:red;font-weight: bold">
         security.provider.2=org.bouncycastle.jsse.provider.BouncyCastleJsseProvider
       </span>
+      {"\n"}
       <span style="color: gray"># Lower priority of existing providers</span>
+      {"\n"}
       security.provider.<span style="color:red;font-weight: bold">3</span>
-      =sun.security.provider.Sun security.provider.
-      <span style="color:red;font-weight: bold">4</span>
-      =sun.security.rsa.SunRsaSign security.provider.
-      <span style="color:red;font-weight: bold">5</span>
-      =com.sun.net.ssl.internal.ssl.Provider security.provider.
-      <span style="color:red;font-weight: bold">6</span>
-      =com.sun.crypto.provider.SunJCE security.provider.
-      <span style="color:red;font-weight: bold">7</span>
-      =sun.security.jgss.SunProvider security.provider.
-      <span style="color:red;font-weight: bold">8</span>
-      =com.sun.security.sasl.Provider security.provider.
-      <span style="color:red;font-weight: bold">9</span>
-      =org.jcp.xml.dsig.internal.dom.XMLDSigRI security.provider.
-      <span style="color:red;font-weight: bold">10</span>
-      =sun.security.smartcardio.SunPCSC
+      =sun.security.provider.Sun{"\n"}
+      security.provider.<span style="color:red;font-weight: bold">4</span>
+      =sun.security.rsa.SunRsaSign{"\n"}
+      security.provider.<span style="color:red;font-weight: bold">5</span>
+      =com.sun.net.ssl.internal.ssl.Provider{"\n"}
+      security.provider.<span style="color:red;font-weight: bold">6</span>
+      =com.sun.crypto.provider.SunJCE{"\n"}
+      security.provider.<span style="color:red;font-weight: bold">7</span>
+      =sun.security.jgss.SunProvider{"\n"}
+      security.provider.<span style="color:red;font-weight: bold">8</span>
+      =com.sun.security.sasl.Provider{"\n"}
+      security.provider.<span style="color:red;font-weight: bold">9</span>
+      =org.jcp.xml.dsig.internal.dom.XMLDSigRI{"\n"}
+      security.provider.<span style="color:red;font-weight: bold">10</span>
+      =sun.security.smartcardio.SunPCSC{"\n"}
       <span style="color: gray">
         # Change default SocketFactory implementation
       </span>
+      {"\n"}
       <span style="color:red;font-weight: bold">
         ssl.SocketFactory.provider=org.bouncycastle.jsse.provider.SSLSocketFactoryImpl
       </span>
