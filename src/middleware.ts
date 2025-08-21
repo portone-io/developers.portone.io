0cebb1310cbcb6f3647d3a6e04eeb7899303c71d
CirnoV (Sickle) <cirnov59@gmail.com>
2025-08-21 16:18:18 +0900

    Vercel Skew Protection 적용

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,20 @@
 import { sentryBeforeResponseMiddleware } from "@sentry/solidstart";
 import { createMiddleware } from "@solidjs/start/middleware";
+import { setCookie } from "vinxi/http";
 
 export default createMiddleware({
+  onRequest: (event) => {
+    if (
+      process.env.VERCEL_SKEW_PROTECTION_ENABLED === "1" &&
+      process.env.VERCEL_DEPLOYMENT_ID
+    ) {
+      setCookie(event.nativeEvent, "__vdpl", process.env.VERCEL_DEPLOYMENT_ID, {
+        httpOnly: true,
+        path: "/",
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+      });
+    }
+  },
   onBeforeResponse: [sentryBeforeResponseMiddleware()],
 });
