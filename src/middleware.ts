4c370d5c122336bd4477f6d6d2c0e958667f0f2a
CirnoV (Sickle) <cirnov59@gmail.com>
2025-08-26 13:45:15 +0900

    vite:preloadError 발생 시 새로고침 하도록 변경

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,6 @@
 import { sentryBeforeResponseMiddleware } from "@sentry/solidstart";
 import { createMiddleware } from "@solidjs/start/middleware";
-import { setCookie } from "vinxi/http";
 
 export default createMiddleware({
-  onRequest: (event) => {
-    if (
-      process.env.VERCEL_SKEW_PROTECTION_ENABLED === "1" &&
-      process.env.VERCEL_DEPLOYMENT_ID
-    ) {
-      setCookie(event.nativeEvent, "__vdpl", process.env.VERCEL_DEPLOYMENT_ID, {
-        httpOnly: true,
-        path: "/_build/assets",
-        sameSite: "strict",
-        secure: true,
-      });
-      setCookie(event.nativeEvent, "__vdpl", process.env.VERCEL_DEPLOYMENT_ID, {
-        httpOnly: true,
-        path: "/_server",
-        sameSite: "strict",
-        secure: true,
-      });
-    }
-  },
   onBeforeResponse: [sentryBeforeResponseMiddleware()],
 });
