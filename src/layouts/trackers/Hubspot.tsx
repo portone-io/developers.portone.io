76c69ad0650a44a37ddc7d42cab56be3dc3c15ca
CirnoV <cirnov59@gmail.com>
2025-09-10 16:44:12 +0900

    Hubspot 트래커 추가

---
+++ src/layouts/trackers/Hubspot.tsx
@@ -0,0 +1,11 @@
+export default function Hubspot() {
+  return (
+    <script
+      type="text/javascript"
+      id="hs-script-loader"
+      async
+      defer
+      src="//js-na2.hs-scripts.com/243569856.js"
+    ></script>
+  );
+}
