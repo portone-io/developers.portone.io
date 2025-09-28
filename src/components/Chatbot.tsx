c9def638dab4d6869c63eae22885f02d809d775a
CirnoV <cirnov59@gmail.com>
2025-09-28 21:00:11 +0900

    챗봇 업데이트 & cleanup 추가

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -1,11 +1,15 @@
 import * as PortOneAIChatbotLoader from "@portone/ai-chatbot-loader";
-import { onMount } from "solid-js";
+import { onCleanup, onMount } from "solid-js";
 
 function Chatbot() {
   onMount(() => {
     PortOneAIChatbotLoader.load({
       baseUrl: "https://ai-chatbot-kappa-blue-portone.vercel.app",
     });
+  });
+
+  onCleanup(() => {
+    PortOneAIChatbotLoader.unload();
   });
 
   return <></>;
