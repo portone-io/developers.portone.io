e6f4036d771a9440a4ace1375cdfcef4a6952382
CirnoV (Sickle) <cirnov59@gmail.com>
2026-02-26 10:55:15 +0900

    V2 Migration 배너 버그 수정

--- src/components/gitbook/V2MigrationBanner.tsx
+++ src/components/gitbook/V2MigrationBanner.tsx
@@ -1,11 +1,18 @@
-import { createMemo, type JSXElement, Show } from "solid-js";
+import { useLocation, useNavigate } from "@solidjs/router";
+import { createMemo, type JSXElement, Show, startTransition } from "solid-js";
 
 import Hint from "~/components/Hint";
+import type { DocsEntry } from "~/content/config";
 import { useSystemVersion } from "~/state/system-version";
 import type { Lang } from "~/type";
 
+const pathMappings = {
+  "/api/rest-v1": "/api/rest-v2",
+};
+
 interface Props {
   lang: Lang;
+  versionVariants?: DocsEntry["versionVariants"];
 }
 
 const messages: Record<
@@ -24,6 +31,8 @@ const messages: Record<
 };
 
 export default function V2MigrationBanner(props: Props): JSXElement {
+  const location = useLocation();
+  const navigate = useNavigate();
   const { systemVersion, setSystemVersion } = useSystemVersion();
   const msg = createMemo(() => messages[props.lang]);
 
@@ -36,7 +45,25 @@ export default function V2MigrationBanner(props: Props): JSXElement {
           </p>
           <button
             type="button"
-            onClick={() => setSystemVersion("v2")}
+            onClick={() => {
+              void startTransition(() => {
+                setSystemVersion("v2");
+              });
+
+              const mappedPath =
+                Object.entries(pathMappings).find(([from]) =>
+                  location.pathname.startsWith(from),
+                )?.[1] ??
+                (props.versionVariants?.v2 && props.versionVariants.v2);
+              if (mappedPath) navigate(mappedPath);
+              else if (
+                ["/opi/", "/sdk/"].some((path) =>
+                  location.pathname.startsWith(path),
+                )
+              )
+                return;
+              else navigate("/");
+            }}
             class="inline-flex items-center gap-1 text-blue-600 font-500 hover:text-blue-700"
           >
             {msg().viewV2Doc}
