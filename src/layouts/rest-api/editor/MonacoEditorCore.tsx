66f9d03d848b21425c067fd44d476edc5c5acb1e
CirnoV (Sickle) <cirnov59@gmail.com>
2026-03-03 12:58:13 +0900

    Shiki, MonacoEditor 초기 번들 문제 해결

---
+++ src/layouts/rest-api/editor/MonacoEditorCore.tsx
@@ -0,0 +1,69 @@
+import { createEffect, createSignal, on, onCleanup, onMount } from "solid-js";
+
+import once from "./misc/once";
+
+type monaco = typeof import("monaco-editor");
+type editor = monaco["editor"];
+export type ITextModel = ReturnType<editor["createModel"]>;
+export type IStandaloneCodeEditor = ReturnType<editor["create"]>;
+
+const doCreateTheme = once();
+
+export interface MonacoEditorProps {
+  init: (monaco: monaco, domElement: HTMLElement) => IStandaloneCodeEditor;
+  onChange?: ((value: string) => void) | undefined;
+  value?: string;
+}
+export default function MonacoEditor(props: MonacoEditorProps) {
+  let divRef: HTMLDivElement | undefined;
+  const [editor, setEditor] = createSignal<IStandaloneCodeEditor>();
+  onMount(() => {
+    const p = Promise.all([
+      import("monaco-editor"),
+      import("./misc/scrollFinished"),
+      import("./misc/install-monaco-worker").then(({ installWorkers }) =>
+        installWorkers(),
+      ),
+    ]).then(async ([monaco, { default: scrollFinished }]) => {
+      doCreateTheme(() => {
+        monaco.editor.defineTheme("portone", {
+          base: "vs",
+          inherit: true,
+          rules: [],
+          colors: {
+            "editorGutter.background": "#f1f5f9",
+          },
+        });
+        monaco.editor.setTheme("portone");
+      });
+      // 브라우저의 smooth scrollTo 중에 dom 수정이 일어나면 스크롤이 도중 끊겨버리기 때문에
+      // 스크롤이 끝났다고 판단됐을 때 monaco editor를 초기화한다.
+      await scrollFinished();
+      const editor = props.init(monaco, divRef!);
+      setEditor(() => editor);
+      const changeEventListener = editor.onDidChangeModelContent(() =>
+        props.onChange?.(editor.getValue()),
+      );
+      return () => {
+        changeEventListener.dispose();
+        editor.dispose();
+      };
+    });
+    onCleanup(() => void p.then((dispose) => dispose()));
+  });
+  createEffect(
+    on(
+      [() => props.value, editor],
+      ([value, editor]) => {
+        if (!editor || value === undefined) return;
+        editor.setValue(value);
+      },
+      { defer: true },
+    ),
+  );
+  return (
+    <div class="relative h-full w-full">
+      <div ref={divRef} class="absolute h-full w-full" />
+    </div>
+  );
+}
