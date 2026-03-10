ad0e15ee42627b5aad603e3d33407f3ecc511e06
CirnoV (Sickle) <cirnov59@gmail.com>
2026-03-10 17:35:10 +0900

    ContentRef 에서 loadDoc query를 사용하도록 수정

--- src/components/gitbook/ContentRef.tsx
+++ src/components/gitbook/ContentRef.tsx
@@ -1,61 +1,37 @@
-import { A, cache, createAsync } from "@solidjs/router";
+import { A, createAsync, query } from "@solidjs/router";
 import { createMemo, Show } from "solid-js";
-import { match } from "ts-pattern";
 
-import { parseDocsFullSlug } from "~/misc/docs";
+import { loadDoc, parseDocsFullSlug } from "~/misc/docs";
 
 interface Props {
   slug: string;
 }
 
-const getOpiEntryData = async (slug: string) => {
-  "use server";
-
-  const { opi } = await import("#content");
-  return (opi as Record<string, (typeof opi)[keyof typeof opi]>)[slug]
-    ?.frontmatter;
-};
-
-const getSdkEntryData = async (slug: string) => {
-  "use server";
-
-  const { sdk } = await import("#content");
-  return (sdk as Record<string, (typeof sdk)[keyof typeof sdk]>)[slug]
-    ?.frontmatter;
-};
-
-const getEntryData = cache(async (fullSlug: string) => {
+const getDocsTitle = query(async (fullSlug: string) => {
   "use server";
 
   const parsedFullSlug = parseDocsFullSlug(fullSlug);
   if (!parsedFullSlug) return;
   const [contentName, slug] = parsedFullSlug;
 
-  return match(contentName)
-    .with("opi", () => {
-      return getOpiEntryData(slug);
-    })
-    .with("sdk", () => {
-      return getSdkEntryData(slug);
-    })
-    .otherwise(() => {
-      return undefined;
-    });
-}, "docs/entry");
+  const docs = await loadDoc(contentName, slug);
+  if (!docs || !("title" in docs.frontmatter)) return;
+
+  return docs.frontmatter.title;
+}, "content-ref/title");
 
 export default function ContentRef(props: Props) {
   const slug = createMemo(() => props.slug);
-  const entryData = createAsync(() => getEntryData(slug()));
-  const title = createMemo(() => entryData()?.title);
+  const title = createAsync(() => getDocsTitle(slug()));
 
   return (
-    <Show when={title()}>
-      <A class="m-4" href={slug()}>
-        <div class="flex items-center justify-between gap-4 border rounded bg-white p-4 transition-transform hover:translate-y-[-2px] hover:text-orange-5 hover:drop-shadow-[0_12px_13px_rgba(0,0,0,0.02)]">
-          <span>{title()}</span>
-          <i class="i-ic-baseline-chevron-right inline-block text-2xl" />
-        </div>
-      </A>
-    </Show>
+    <A class="m-4" href={slug()}>
+      <div class="flex items-center justify-between gap-4 border rounded bg-white p-4 transition-transform hover:translate-y-[-2px] hover:text-orange-5 hover:drop-shadow-[0_12px_13px_rgba(0,0,0,0.02)]">
+        <Show when={title()} fallback={<span>{slug()}</span>}>
+          {(title) => <span>{title()}</span>}
+        </Show>
+        <i class="i-ic-baseline-chevron-right inline-block text-2xl" />
+      </div>
+    </A>
   );
 }
