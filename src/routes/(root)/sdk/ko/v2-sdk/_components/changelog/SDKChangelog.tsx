df8aa5503eb73f835d482b5d17a487764dcbd16a
CirnoV (Sickle) <cirnov59@gmail.com>
2026-04-10 14:51:56 +0900

    lint 오류 해결

--- src/routes/(root)/sdk/ko/v2-sdk/_components/changelog/SDKChangelog.tsx
+++ src/routes/(root)/sdk/ko/v2-sdk/_components/changelog/SDKChangelog.tsx
@@ -2,9 +2,13 @@ import "~/components/prose.css";
 
 import { createAsync } from "@solidjs/router";
 import { query } from "@solidjs/router";
+
 import { loadChangelog as loadChangelogFromServer } from "./loadChangelog";
 
-const loadChangelog = query(loadChangelogFromServer, "docs/v2-payments/v2-sdk/changelog")
+const loadChangelog = query(
+  loadChangelogFromServer,
+  "docs/v2-payments/v2-sdk/changelog",
+);
 
 export default function SDKChangelog() {
   const html = createAsync(() => loadChangelog());
