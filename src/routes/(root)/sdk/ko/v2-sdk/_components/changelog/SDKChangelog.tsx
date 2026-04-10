d780bbbfef1e221e3e6e680f6a70de4f9f3bfa2d
CirnoV (Sickle) <cirnov59@gmail.com>
2026-04-10 13:57:59 +0900

    빌드 오류 해결

--- src/routes/(root)/sdk/ko/v2-sdk/_components/changelog/SDKChangelog.tsx
+++ src/routes/(root)/sdk/ko/v2-sdk/_components/changelog/SDKChangelog.tsx
@@ -1,8 +1,10 @@
 import "~/components/prose.css";
 
 import { createAsync } from "@solidjs/router";
+import { query } from "@solidjs/router";
+import { loadChangelog as loadChangelogFromServer } from "./loadChangelog";
 
-import { loadChangelog } from "./loadChangelog";
+const loadChangelog = query(loadChangelogFromServer, "docs/v2-payments/v2-sdk/changelog")
 
 export default function SDKChangelog() {
   const html = createAsync(() => loadChangelog());
